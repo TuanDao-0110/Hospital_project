a96bbe226721ce78ccf90254666acbf60caed567
tuan <rickytuan1991@gmail.com>
2022-10-29 22:47:20 +0300

    send to server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hospital = require('./hospital_class_porperty')
 const data = require('./hospital_information.json')
-
+const http = require('http')
+const port = 8080;
 // 1. create newData with only data we need
 let newDatas = []
 
@@ -18,6 +19,16 @@ newDatas.map((item, index) => {
     (item['organisaatio'].toLowerCase().includes('oy')) && (hospitalPrivate.push(item))
 })
 
-//   2.1 now we can see all the private name: 
-console.log(hospitalPrivate)
+//   2.1 now we can send data all the private name: 
 
+const server = http.createServer((req, res) => {
+
+    const newUrl = new URL(`http://${req.headers.host}${req.url}`)
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({ status: 200, msg: 'success', hospitalPrivate }))
+})
+server.listen(port, () => {
+    console.log(`listening on port : ${port}.....`)
+})
